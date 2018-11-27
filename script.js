a970b4c8676b74e7fd561d04e5ca4152c0aded6a
Godsgift Akari <giftakari@yahoo.com>
2018-11-28 00:31:24 +1100

    First Commit

---
+++ script.js
@@ -0,0 +1,20 @@
+let countDown;
+let timeInterval;
+const endDate = document.querySelector('input[name ="endDate"]');
+const clock = document.querySelector("#clock");
+const daysSpan = clock.querySelector(".days");
+const hoursSpan = clock.querySelector(".hours");
+const minutesSpan = clock.querySelector(".minutes");
+const   secondSpan = clock.querySelector(".second");
+
+endDate.addEventListener("change",function(e){
+    e.preventDefault();
+    clearInterval(timeInterval);
+    console.dir(this);
+    const endDate = new Date(this.value);
+})
+
+
+
+
+//console.log (endDate);
